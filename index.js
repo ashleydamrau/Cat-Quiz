808cdfe5f57978b843ed29970282a3d1b3e2fbda
Ashley Damrau <ashleygirl789@yahoo.com>
2024-11-22 16:06:45 -0800

    added a result page

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function startQuiz() {
-  window.location.href = 'quiz1.html';
+  window.location.href = "quiz1.html";
 }
+
+const choices = {
+  A: 0,
+  B: 0,
+  C: 0,
+  D: 0
+};
+
+const questionButtons = document.querySelectorAll(".questionButton");
+
+questionButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const value = event.target.getAttribute("data-value");
+
+    if (choices[value] !== undefined) {
+      choices[value]++;
+    }
+
+    localStorage.setItem("quizChoices", JSON.stringify(choices));
+
+    const parentDiv = event.target.parentElement;
+    const siblingButtons = parentDiv.querySelectorAll(".questionButtons");
+    siblingButtons.forEach((btn) => btn.disabled = true);
+
+    document.querySelector(".nextButton").disabled = false;
+  });
+});
+
